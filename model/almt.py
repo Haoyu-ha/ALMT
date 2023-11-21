9a34b685d0b573b9af3ea7b2067af218de42ed62
haoyu.pers <912540177@qq.com>
2023-11-21 20:36:24 +0800

    Add files via upload

---
+++ model/almt.py
@@ -0,0 +1,83 @@
+
+'''
+* @name: ALMT
+* @description: Implementation of ALMT
+'''
+
+import torch
+from torch import nn
+from .almt_layer import Transformer, CrossTransformer, HhyperLearningEncoder
+from .bert import BertTextEncoder
+from einops import repeat
+
+
+class ALMT(nn.Module):
+    def __init__(self, dataset, AHL_depth=3, fusion_layer_depth=2, bert_pretrained='bert-base-uncased'):
+        super(ALMT, self).__init__()
+
+        self.h_hyper = nn.Parameter(torch.ones(1, 8, 128))
+
+        self.bertmodel = BertTextEncoder(use_finetune=True, transformers='bert', pretrained=bert_pretrained)
+
+        # mosi
+        if dataset == 'mosi':
+            self.proj_l0 = nn.Linear(768, 128)
+            self.proj_a0 = nn.Linear(5, 128)
+            self.proj_v0 = nn.Linear(20, 128)
+        elif dataset == 'mosei':
+            self.proj_l0 = nn.Linear(768, 128)
+            self.proj_a0 = nn.Linear(74, 128)
+            self.proj_v0 = nn.Linear(35, 128)
+        elif dataset == 'sims':
+            self.proj_l0 = nn.Linear(768, 128)
+            self.proj_a0 = nn.Linear(33, 128)
+            self.proj_v0 = nn.Linear(709, 128)
+        else:
+            exit(0)
+
+
+        self.proj_l = Transformer(num_frames=50, save_hidden=False, token_len=8, dim=128, depth=1, heads=8, mlp_dim=128)
+        self.proj_a = Transformer(num_frames=50, save_hidden=False, token_len=8, dim=128, depth=1, heads=8, mlp_dim=128)
+        self.proj_v = Transformer(num_frames=50, save_hidden=False, token_len=8, dim=128, depth=1, heads=8, mlp_dim=128)
+
+        self.text_encoder = Transformer(num_frames=8, save_hidden=True, token_len=None, dim=128, depth=AHL_depth-1, heads=8, mlp_dim=128)
+        self.h_hyper_layer = HhyperLearningEncoder(dim=128, depth=AHL_depth, heads=8, dim_head=16, dropout = 0.)
+        self.fusion_layer = CrossTransformer(source_num_frames=8, tgt_num_frames=8, dim=128, depth=fusion_layer_depth, heads=8, mlp_dim=128)
+
+        self.cls_head = nn.Sequential(
+            nn.Linear(128, 1)
+        )
+
+
+    def forward(self, x_visual, x_audio, x_text):
+        b = x_visual.size(0)
+
+        h_hyper = repeat(self.h_hyper, '1 n d -> b n d', b = b)
+
+        x_text = self.bertmodel(x_text)
+
+        x_visual = self.proj_v0(x_visual)
+        x_audio = self.proj_a0(x_audio)
+        x_text = self.proj_l0(x_text)
+
+        h_v = self.proj_v(x_visual)[:, :8]
+        h_a = self.proj_a(x_audio)[:, :8]
+        h_t = self.proj_l(x_text)[:, :8]
+
+        h_t_list = self.text_encoder(h_t)
+
+        h_hyper = self.h_hyper_layer(h_t_list, h_a, h_v, h_hyper)
+        feat = self.fusion_layer(h_hyper, h_t_list[-1])[:, 0]
+        cls_output = self.cls_head(feat)
+
+        return cls_output
+
+
+def build_model(opt, mode=None):
+    if opt.datasetName == 'sims':
+        l_pretrained='bert-base-chinese'
+    else:
+        l_pretrained='bert-base-uncased'
+    model = ALMT(dataset = opt.datasetName, fusion_layer_depth=opt.fusion_layer_depth, bert_pretrained = l_pretrained)
+
+    return model
---
+++ model/almt_layer.py
@@ -0,0 +1,285 @@
+'''
+* @name: almt_layer.py
+* @description: Basic layer of ALMT. Note: Some code references Vision Transformer (VIT, https://github.com/gupta-abhay/pytorch-vit).
+'''
+
+import torch
+from torch import nn, einsum
+from einops import rearrange, repeat
+
+
+def pair(t):
+    return t if isinstance(t, tuple) else (t, t)
+
+
+class PreNormForward(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+
+class PreNormAttention(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm_q = nn.LayerNorm(dim)
+        self.norm_k = nn.LayerNorm(dim)
+        self.norm_v = nn.LayerNorm(dim)
+        self.fn = fn
+
+    def forward(self, q, k, v, **kwargs):
+        q = self.norm_q(q)
+        k = self.norm_k(k)
+        v = self.norm_v(v)
+
+        return self.fn(q, k, v)
+
+class PreNormAHL(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm1 = nn.LayerNorm(dim)
+        self.norm2 = nn.LayerNorm(dim)
+        self.norm3 = nn.LayerNorm(dim)
+        self.norm4 = nn.LayerNorm(dim)
+        self.fn = fn
+
+    def forward(self, h_t, h_a, h_v, h_hyper):
+        h_t = self.norm1(h_t)
+        h_a = self.norm2(h_a)
+        h_v = self.norm3(h_v)
+        h_hyper = self.norm4(h_hyper)
+
+        return self.fn(h_t, h_a, h_v, h_hyper)
+
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout = 0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+    def forward(self, x):
+        return self.net(x)
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.attend = nn.Softmax(dim = -1)
+        self.to_q = nn.Linear(dim, inner_dim, bias=False)
+        self.to_k = nn.Linear(dim, inner_dim, bias=False)
+        self.to_v = nn.Linear(dim, inner_dim, bias=False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, q, k, v):
+        b, n, _, h = *q.shape, self.heads
+        q = self.to_q(q)
+        k = self.to_k(k)
+        v = self.to_v(v)
+
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=h), (q, k, v))
+        dots = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
+
+        attn = self.attend(dots)
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+
+        return self.to_out(out)
+
+
+class HhyperLearningLayer(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.attend = nn.Softmax(dim = -1)
+        self.to_q = nn.Linear(dim, inner_dim, bias=False)
+        self.to_k_ta = nn.Linear(dim, inner_dim, bias=False)
+        self.to_k_tv = nn.Linear(dim, inner_dim, bias=False)
+        self.to_v_ta = nn.Linear(dim, inner_dim, bias=False)
+        self.to_v_tv = nn.Linear(dim, inner_dim, bias=False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim, bias=True),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, h_t, h_a, h_v, h_hyper):
+        b, n, _, h = *h_t.shape, self.heads
+
+        q = self.to_q(h_t)
+        k_ta = self.to_k_ta(h_a)
+        k_tv = self.to_k_tv(h_v)
+        v_ta = self.to_v_ta(h_a)
+        v_tv = self.to_v_tv(h_v)
+
+        q, k_ta, k_tv, v_ta, v_tv = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=h), (q, k_ta, k_tv, v_ta, v_tv))
+
+        dots_ta = einsum('b h i d, b h j d -> b h i j', q, k_ta) * self.scale
+        attn_ta = self.attend(dots_ta)
+        out_ta = einsum('b h i j, b h j d -> b h i d', attn_ta, v_ta)
+        out_ta = rearrange(out_ta, 'b h n d -> b n (h d)')
+
+        dots_tv = einsum('b h i d, b h j d -> b h i j', q, k_tv) * self.scale
+        attn_tv = self.attend(dots_tv)
+        out_tv = einsum('b h i j, b h j d -> b h i d', attn_tv, v_tv)
+        out_tv = rearrange(out_tv, 'b h n d -> b n (h d)')
+
+        h_hyper_shift = self.to_out(out_ta + out_tv)
+        h_hyper += h_hyper_shift
+
+        return h_hyper
+
+
+class HhyperLearningEncoder(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, dropout = 0.):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                PreNormAHL(dim, HhyperLearningLayer(dim, heads = heads, dim_head = dim_head, dropout = dropout))
+            ]))
+
+    def forward(self, h_t_list, h_a, h_v, h_hyper):
+        for i, attn in enumerate(self.layers):
+            h_hyper = attn[0](h_t_list[i], h_a, h_v, h_hyper)
+        return h_hyper
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                PreNormAttention(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
+                PreNormForward(dim, FeedForward(dim, mlp_dim, dropout = dropout))
+            ]))
+
+    def forward(self, x, save_hidden=False):
+        if save_hidden == True:
+            hidden_list = []
+            hidden_list.append(x)
+            for attn, ff in self.layers:
+                x = attn(x, x, x) + x
+                x = ff(x) + x
+                hidden_list.append(x)
+            return hidden_list
+        else:
+            for attn, ff in self.layers:
+                x = attn(x, x, x) + x
+                x = ff(x) + x
+            return x
+
+
+class CrossTransformerEncoder(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                PreNormAttention(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
+                PreNormForward(dim, FeedForward(dim, mlp_dim, dropout = dropout))
+            ]))
+
+    def forward(self, source_x, target_x):
+        for attn, ff in self.layers:
+            target_x_tmp = attn(target_x, source_x, source_x)
+            target_x = target_x_tmp + target_x
+            target_x = ff(target_x) + target_x
+        return target_x
+
+
+
+class Transformer(nn.Module):
+    def __init__(self, *, num_frames, token_len, save_hidden, dim, depth, heads, mlp_dim, pool = 'cls', channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0.):
+        super().__init__()
+
+        self.token_len = token_len
+        self.save_hidden = save_hidden
+
+        if token_len is not None:
+            self.pos_embedding = nn.Parameter(torch.randn(1, num_frames + token_len, dim))
+            self.extra_token = nn.Parameter(torch.zeros(1, token_len, dim))
+        else:
+             self.pos_embedding = nn.Parameter(torch.randn(1, num_frames, dim))
+             self.extra_token = None
+
+        self.dropout = nn.Dropout(emb_dropout)
+
+        self.encoder = TransformerEncoder(dim, depth, heads, dim_head, mlp_dim, dropout)
+
+        self.pool = pool
+        self.to_latent = nn.Identity()
+
+
+    def forward(self, x):
+        b, n, _ = x.shape
+
+        if self.token_len is not None:
+            extra_token = repeat(self.extra_token, '1 n d -> b n d', b = b)
+            x = torch.cat((extra_token, x), dim=1)
+            x = x + self.pos_embedding[:, :n+self.token_len]
+        else:
+            x = x + self.pos_embedding[:, :n]
+
+        x = self.dropout(x)
+        x = self.encoder(x, self.save_hidden)
+
+        return x
+
+
+class CrossTransformer(nn.Module):
+    def __init__(self, *, source_num_frames, tgt_num_frames, dim, depth, heads, mlp_dim, pool = 'cls', dim_head = 64, dropout = 0., emb_dropout = 0.):
+        super().__init__()
+
+        self.pos_embedding_s = nn.Parameter(torch.randn(1, source_num_frames + 1, dim))
+        self.pos_embedding_t = nn.Parameter(torch.randn(1, tgt_num_frames + 1, dim))
+        self.extra_token = nn.Parameter(torch.zeros(1, 1, dim))
+
+        self.dropout = nn.Dropout(emb_dropout)
+
+        self.CrossTransformerEncoder = CrossTransformerEncoder(dim, depth, heads, dim_head, mlp_dim, dropout)
+
+        self.pool = pool
+
+    def forward(self, source_x, target_x):
+        b, n_s, _ = source_x.shape
+        b, n_t, _ = target_x.shape
+
+        extra_token = repeat(self.extra_token, '1 1 d -> b 1 d', b = b)
+
+        source_x = torch.cat((extra_token, source_x), dim=1)
+        source_x = source_x + self.pos_embedding_s[:, : n_s+1]
+
+        target_x = torch.cat((extra_token, target_x), dim=1)
+        target_x = target_x + self.pos_embedding_t[:, : n_t+1]
+
+        source_x = self.dropout(source_x)
+        target_x = self.dropout(target_x)
+
+        x_s2t = self.CrossTransformerEncoder(source_x, target_x)
+
+        return x_s2t
+
